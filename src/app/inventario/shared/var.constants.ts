9759dcee9d80f6c1bc9a41187e369c5d208f07aa
Luis Lizana <luisferliza@gmail.com>
2021-06-01 12:09:59 -0600

    Corrección de URL de Inventarios

--- src/app/inventario/shared/var.constants.ts
+++ src/app/inventario/shared/var.constants.ts
@@ -1,3 +1,3 @@
-export const INVENTARIO_HOST = 'http://localhost:8076';
+export const INVENTARIO_HOST = 'http://10.50.47.220/gateway/pp-inventario';
 export const INVERSIONES_HOST = 'http://localhost:8085';
 export const USUARIOS_HOST = 'https://10.50.47.220';
